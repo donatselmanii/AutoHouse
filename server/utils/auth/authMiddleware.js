5b15c18d7baccbacab1f85af761c1db7a4b9b9e2
donatselmanii <124844058+donatselmanii@users.noreply.github.com>
2024-02-06 01:20:39 +0100

    Update authMiddleware.js for session regeneration in case the server stops working.

--- server/utils/auth/authMiddleware.js
+++ server/utils/auth/authMiddleware.js
@@ -1,13 +1,42 @@
+const jwt = require('jsonwebtoken');
+const { errorHandler } = require('../error');
+
 const checkUserRole = (allowedRoles) => {
-    return (req, res, next) => {
-        if (req.session && req.session.userRole) {
-            if (allowedRoles.includes(req.session.userRole)) {
-                next();
+    return async (req, res, next) => {
+        try {
+            if (req.session && req.session.userRole) {
+                if (allowedRoles.includes(req.session.userRole)) {
+                    next();
+                } else {
+                    res.status(403).json({ error: 'Forbidden: Insufficient permissions' });
+                }
             } else {
-                res.status(403).json({ error: 'Forbidden: Insufficient permissions' });
+                const token = req.cookies.token;
+
+                if (!token) {
+                    const unauthorizedError = errorHandler(401, 'Unauthorized: User not logged in');
+                    return next(unauthorizedError);
+                }
+
+                jwt.verify(token, "7B&6$QyMv48gZ*aR@n!PbUt2Lj%zWsHs", (err, user) => {
+                    if (err) {
+                        const forbiddenError = errorHandler(403, 'Forbidden: Invalid token');
+                        return next(forbiddenError);
+                    }
+
+                    req.session.userRole = user.roleId;
+                    req.session.save();
+
+                    if (allowedRoles.includes(req.session.userRole)) {                    
+                        next();
+                    } else {
+                        res.status(403).json({ error: 'Forbidden: Insufficient permissions' });
+                    }
+                });
             }
-        } else {
-            res.status(401).json({ error: 'Unauthorized: User not logged in' });
+        } catch (error) {
+            console.error('Error in checkUserRole middleware:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
         }
     };
 };
